086927e3c4548bfa58691b3b178cd8cf8f1884db
Igor <0277203@gmail.com>
2024-12-01 20:23:39 +0200

    Enhance image processing in filter_map.py using OpenCV for noise reduction and contrast adjustment

--- filter_map.py
+++ filter_map.py
@@ -2,6 +2,7 @@ import os
 from PIL import Image
 import numpy as np
 from concurrent.futures import ThreadPoolExecutor
+import cv2
 
 def is_black_image(image_path):
     image = Image.open(image_path)
@@ -21,12 +22,25 @@ def process_image(filename, directory):
         png_path = os.path.join(directory, filename)
         dat_path = os.path.join(directory, filename.replace('.png', '.dat'))
 
-        if is_black_image(png_path) or has_transparent_pixels(png_path) > 0.25:
+        if is_black_image(png_path) or has_transparent_pixels(png_path) > 0.33:
             if os.path.exists(png_path):
                 os.remove(png_path)
             if os.path.exists(dat_path):
                 os.remove(dat_path)
             print(f"Deleted: {png_path} and {dat_path}")
+        else:
+            # Перевести зображення в ЧБ
+            image = cv2.imread(png_path, cv2.IMREAD_GRAYSCALE)
+            
+            # Видалити шум
+            image = cv2.medianBlur(image, 3)
+            
+            # Відрегулювати яскравість та контрастність
+            image = cv2.convertScaleAbs(image, alpha=1.5, beta=20)
+            
+            # Зберегти результат
+            cv2.imwrite(png_path, image)
+            print(f"Processed and saved: {png_path}")
 
 def delete_black_images_and_pairs(directory):
     with ThreadPoolExecutor() as executor:
@@ -35,5 +49,4 @@ def delete_black_images_and_pairs(directory):
         for future in futures:
             future.result()
 
-# Виклик функції для папки ./img
-delete_black_images_and_pairs('./map')
+delete_black_images_and_pairs('./img')
